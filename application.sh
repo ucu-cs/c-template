f13028da4fdbefcc88db0c1c8232b415f46c4395
Mykola Morgunenko <Olli25010@gmail.com>
2020-09-09 15:01:50 +0300

    replaced README gitignore, inc, src, main\nrm workflows\nadded application.sh, media, scripts

---
+++ application.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# OPTIONAL
+
+# Shell script that can be useful for automation some processes.
+# For example: 
+###
+#
+#FILE_IN=${argc}[1]
+#FILE_OUT=${argc}[2]
+#
+#mkdir -p build
+#mkdir -p bin
+#
+#cd build
+#cmake -G"Unix Makefiles" ..
+#make
+#cd ../bin/
+#chmod u+x main
